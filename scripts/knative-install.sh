3937323ca9d0a2c1e718e80e85e84fc99e953be3
Flávio Ramalho <framalho@suse.com>
2021-03-12 16:52:42 +0100

    ci: Change knative magic dns from xip.io to nip.io
    Maybe nip.io is more reliable.

--- scripts/knative-install.sh
+++ scripts/knative-install.sh
@@ -11,6 +11,6 @@ export KNATIVE_VERSION=v0.21.0
 kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
 kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
 kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
-kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-default-domain.yaml
+curl -L https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-default-domain.yaml | sed 's/xip/nip/g' | kubectl apply -f -
 
 kubectl wait --for=condition=complete --timeout=600s job/default-domain -n knative-serving
