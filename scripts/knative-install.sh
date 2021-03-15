61029fcd3cd256b94e691d87727d15fe23a13033
Flávio Ramalho <framalho@suse.com>
2021-03-15 12:12:50 +0100

    fix: set knative revision timeout to 60 seconds
    Set the knative default revision timeout from 5 minutes to 1 minute as this
    value is used as temrinationGracePeriod on the pod and it is making deleting
    knative services take at least 5 minutes.

--- scripts/knative-install.sh
+++ scripts/knative-install.sh
@@ -13,4 +13,19 @@ kubectl apply --filename https://github.com/knative/serving/releases/download/${
 kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
 curl -L https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-default-domain.yaml | sed 's/xip/nip/g' | kubectl apply -f -
 
+# Set the knative default revision timeout from 5 minutes to 1 minute as this
+# value is used as temrinationGracePeriod on the pod and it is making deleting
+# knative services take at least 5 minutes.
+# There is a issue upstream about this and it should be fixed
+# see: https://github.com/knative/serving/issues/3355
+cat << EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+ name: config-defaults
+ namespace: knative-serving
+data:
+  revision-timeout-seconds: "60"
+EOF
+
 kubectl wait --for=condition=complete --timeout=600s job/default-domain -n knative-serving
