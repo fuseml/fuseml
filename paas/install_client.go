06e02e09252cb0c15a836c28b4c2095604445173
Jiří Suchomel <jiri.suchomel@suse.com>
2021-05-19 20:08:35 +0200

    fix: Pass TEKTON_DASHBOARD_URL value to fuseml-core service
    If not present as environment variable (TEKTON_DASHBOARD_URL),
    it's gonna be constructed from the domain that is used for installation.

--- paas/install_client.go
+++ paas/install_client.go
@@ -112,9 +112,9 @@ func (c *InstallClient) Install(cmd *cobra.Command, options *kubernetes.Installa
 		&deployments.Workloads{Timeout: DefaultTimeoutSec},
 		&deployments.MLflow{Timeout: DefaultTimeoutSec},
 		&deployments.Gitea{Timeout: DefaultTimeoutSec},
-		&deployments.Core{Timeout: DefaultTimeoutSec},
 		&deployments.Registry{Timeout: DefaultTimeoutSec},
 		&deployments.Tekton{Timeout: DefaultTimeoutSec},
+		&deployments.Core{Timeout: DefaultTimeoutSec},
 	} {
 		details.Info("deploy", "Deployment", deployment.ID())
 
