a55e9ff187f3178e2c59cfac9814ecb68e528b6b
Jiří Suchomel <jiri.suchomel@suse.com>
2021-05-28 12:28:10 +0200

    fix: Download and unzip fuseml-core client for Windows
    Unlike other systems, archive for windows is just zip.

--- helpers/downloader.go
+++ helpers/downloader.go
@@ -2,9 +2,12 @@ package helpers
 
 import (
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/pkg/errors"
 )
 
 // DownloadFile downloads the given url as a file with "name" under "directory"
@@ -36,4 +39,30 @@ func DownloadAndUntar(url, targetDir string) error {
 	defer resp.Body.Close()
 
 	return UntarStream(resp.Body, targetDir)
+}
+
+// DownloadAndUnzip downloads the archive from given url and decopresses it under the target directory
+func DownloadAndUnzip(url, targetDir string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	tmpDir, err := ioutil.TempDir("", "fuseml-core")
+	if err != nil {
+		return errors.Wrap(err, "can't create temp directory for download")
+	}
+	defer os.Remove(tmpDir)
+
+	tmpPath := path.Join(tmpDir, "archive.zip")
+	out, err := os.Create(tmpPath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+
+	return Unzip(tmpPath, targetDir)
 }
