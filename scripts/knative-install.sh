7ad2bdc1d76e07aacefe2b2538c846747d3c373d
Flávio Ramalho <framalho@suse.com>
2021-07-15 12:30:24 +0200

    feat: upgrade knative version to 0.24.0

--- scripts/knative-install.sh
+++ scripts/knative-install.sh
@@ -5,29 +5,17 @@ if kubectl get svc controller -n knative-serving >/dev/null 2>&1 ; then
   exit
 fi
 
-export KNATIVE_VERSION=v0.21.0
+export KNATIVE_VERSION=v0.24.0
 
 # Install Knative
 kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
 kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
-kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
-
 kubectl wait --for=condition=available --timeout=600s deployment --all -n knative-serving
 
-# Set the knative default revision timeout from 5 minutes to 1 minute as this
-# value is used as temrinationGracePeriod on the pod and it is making deleting
-# knative services take at least 5 minutes.
-# There is a issue upstream about this and it should be fixed
-# see: https://github.com/knative/serving/issues/3355
-cat << EOF | kubectl apply -f -
-apiVersion: v1
-kind: ConfigMap
-metadata:
- name: config-defaults
- namespace: knative-serving
-data:
-  revision-timeout-seconds: "60"
-EOF
+# Configure Istio as ingress provider for Knative
+kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
+kubectl wait --for=condition=available --timeout=600s deployment -l networking.knative.dev/ingress-provider=istio -n knative-serving
+
 
 # Patch the KNative domain configuration with the domain pointing to the Istio ingress gateway.
 # This is computed as follows:
